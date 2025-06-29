4ffc1417f86b4646bf3744f699a5f924f509c180
Praveen <praveenapk18119@gmail.com>
2025-06-29 18:05:35 +0530

    code modified

--- QR Generator/js/script.js
+++ QR Generator/js/script.js
@@ -1,50 +1,54 @@
-const frm= document.querySelector("#frm");
-const output =document .querySelector(".output");
-const spinner=document.querySelector(".loader");
-const qrcodeElement=document.querySelector("#qrcode");
-const btnSave=document.querySelector("#btns");
+const frm = document.querySelector("#frm");
+const output = document.querySelector(".output");
+const spinner = document.querySelector(".loader");
+const qrcodeElement = document.querySelector("#qrcode");
+const btnSave = document.querySelector("#btns");
 
-function generateQR(e){
-    e.preventDefault();
-    const url=document.querySelector("#url").value;
-    const size=document.querySelector("#size").value;
-    const clrLight=document.querySelector("#lightcolor").value;
-    const clrDark=document.querySelector("#darkcolor").value;
+function generateQR(e) {
+  e.preventDefault();
+  const url = document.querySelector("#url").value;
+  const size = document.querySelector("#size").value;
+  const clrLight = document.querySelector("#lightcolor").value;
+  const clrDark = document.querySelector("#darkcolor").value;
 
-   if (url===""){
+  if (url === "") {
     alert("enter your website URL");
-   }
-   else{
-     spinner.style.display='flex';
-   
- setTimeout(()=>{
- 
-    spinner.style.display="none";
-    qrcodeElement.innerHTML=" ";
-    const qrcode=new QRCode('qrcode',{
+  } else {
+    spinner.style.display = "flex";
+
+    setTimeout(() => {
+      spinner.style.display = "none";
+      qrcodeElement.innerHTML = " ";
+      // this will automatically injects (replace) the qr object(image data) to the "qrcode" class id
+      const qrcode = new QRCode("qrcode", {
         text: url,
         width: size,
         height: size,
-        colorDark : clrDark,
-        colorLight : clrLight,
-        correctLevel : QRCode.CorrectLevel.H
+        colorDark: clrDark,
+        colorLight: clrLight,
+        correctLevel: QRCode.CorrectLevel.H,
       });
- },1000);
 
- createDownloadLink();
+      setTimeout(() => {
+        createDownloadLink();
+      }, 100);
+    }, 3000);
 
-}}
-
-
-frm.addEventListener('submit',generateQR);
-
-function createDownloadLink(){
-    const imgSrc=qrcodeElement.querySelector('img').src;
-    btnSave.href=imgSrc;
+    // createDownloadLink(); this will execute before qr generated
+  }
 }
 
-btnSave.addEventListener('click',()=>{
-    setTimeout(()=>{
-        btnSave.download='qrcode';
-    },50);
-});
+frm.addEventListener("submit", generateQR);
+
+function createDownloadLink() {
+  const imgSrc = qrcodeElement.querySelector("img").src;
+  console.log(imgSrc);
+  btnSave.href = imgSrc;
+}
+
+btnSave.addEventListener("click", downloadQR);
+
+function downloadQR() {
+  alert("image is downloading..");
+  btnSave.download = "qrcode";
+}
